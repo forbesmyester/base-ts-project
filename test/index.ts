b3a87f20758d4886e896c30e4cdce64b9517e25b
Matthew Forrester <matthew.forrester@speechmarks.com>
2016-08-05 21:23:44 +0100

    tidy test

--- test/index.ts
+++ test/index.ts
@@ -2,24 +2,13 @@ import math from '../src/index';
 import * as Types from '../src/types';
 import test from 'ava';
 
-test('Can do math easy', function(t) {
-    // t.deepEqual(
-    //     math(1, 2),
-    //     {
-    //         a: 1,
-    //         b: 2,
-    //         equals: 3
-    //     }
-    // );
-});
-
-test('Can do math difficult', function(t) {
+test('Can do math', function(t) {
     t.deepEqual(
-        math(2, 3),
+        math(1, 2),
         {
-            a: 2,
-            b: 3,
-            equals: 5
+            a: 1,
+            b: 2,
+            equals: 3
         }
     );
 });
